6fd8c5f0ed2287027b2821aff970238a6c6df784
Darina Jurovskaja <darina@Darinas-MacBook-Pro.local>
2024-05-28 15:26:16 +0100

    Adds GET request for all possible to-date endpoints

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const { getTopics } = require("./controllers/topics.controllers")
+const { getEndpoints } = require("./controllers/api.controllers")
 
 const express = require("express")
 
 const app = express()
 
+app.use(express.json())
+
 app.get("/api/topics", getTopics)
+
+app.get("/api", getEndpoints)
 
 app.use((err, req, res, next) => {
     res.status(500).send('Server Error! :(')
