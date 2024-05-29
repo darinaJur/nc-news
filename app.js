67cdd8ea256c6088ba6b6f7bbbef56cb1b54c3c3
Darina Jurovskaja <darina@Darinas-MacBook-Pro.local>
2024-05-29 12:01:55 +0100

    Adds a GET request to get articles by ID and updates endpoints JSON file

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const { getTopics } = require("./controllers/topics.controllers")
 const { getEndpoints } = require("./controllers/api.controllers")
+const { getArticleById } = require("./controllers/articles.controllers")
 
 const express = require("express")
 
 const app = express()
 
-app.use(express.json())
-
 app.get("/api/topics", getTopics)
 
 app.get("/api", getEndpoints)
 
-app.use((err, req, res, next) => {
-    res.status(500).send('Server Error! :(')
+app.get("/api/articles/:article_id", getArticleById)
+
+app.all('*', (req, res) => {
+    res.status(404).send({msg: "The request path does not exist"})
 })
+
+app.use((err, req, res, next) => {
+    if (err.msg) {
+        res.status(err.status).send({msg: err.msg})
+    } else { 
+    res.status(500).send('Server Error! :(')
+    }
+})
+
 
 
 
