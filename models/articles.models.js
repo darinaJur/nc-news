444f36e2270a94d5a096113767e38279d38e6f25
Darina Jurovskaja <darina@Darinas-MacBook-Pro.local>
2024-05-30 15:40:52 +0100

    Adds testing for GET request, when article ID is valid but has no associated comments

--- models/articles.models.js
+++ models/articles.models.js
@@ -25,15 +25,21 @@ exports.selectArticles = () => {
     })
 }
 
+exports.checkArticleExists = (article_id) => {
+    return db.query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .then(({ rows }) => {
+        if(!rows.length) {
+            return Promise.reject({ status: 404, msg: 'Article not found :(' })
+        }
+    })
+}
 exports.selectArticleCommentsById = (article_id) => {
     return db.query(`SELECT * FROM comments
         WHERE article_id = $1
         ORDER BY created_at DESC;`,
         [article_id])
     .then(({rows}) => {
-        if (!rows.length) {
-            return Promise.reject({status: 404, msg: 'Article not found and comments are unavailable'})
-        } else return rows
+        return rows
     })
 }
 
