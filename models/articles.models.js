3e46fa5ce83a04a0390f18cec34dbfc97fa673f2
Darina Jurovskaja <darina@Darinas-MacBook-Pro.local>
2024-05-31 13:58:30 +0100

    Updates GET method to respond with extra property of comment count of the article

--- models/articles.models.js
+++ models/articles.models.js
@@ -1,20 +1,36 @@
 const db = require("../db/connection");
 
+// exports.selectArticleById = (article_id) => {
+//   return db
+//     .query(
+//       `SELECT * FROM articles
+//     WHERE article_id = $1`,
+//       [article_id]
+//     )
+//     .then(({ rows }) => {
+//       if (rows.length === 0) {
+//         return Promise.reject({ status: 404, msg: "Article not found" });
+//       } else {
+//         return rows[0];
+//       }
+//     });
+// };
+
 exports.selectArticleById = (article_id) => {
-  return db
-    .query(
-      `SELECT * FROM articles
-    WHERE article_id = $1`,
-      [article_id]
-    )
-    .then(({ rows }) => {
-      if (rows.length === 0) {
-        return Promise.reject({ status: 404, msg: "Article not found" });
-      } else {
-        return rows[0];
-      }
-    });
-};
+    return db
+      .query(
+        `SELECT articles.*, CAST((SELECT COUNT(*) FROM comments WHERE comments.article_id = articles.article_id) AS INTEGER) AS comment_count FROM articles
+      WHERE article_id = $1;`,
+        [article_id]
+      )
+      .then(({ rows }) => {
+        if (rows.length === 0) {
+          return Promise.reject({ status: 404, msg: "Article not found" });
+        } else {
+          return rows[0];
+        }
+      });
+  };
 
 exports.selectArticles = (topic) => {
   const validTopic = ["cats", "mitch"];
