fb84d2d3c151219c1eb61528b45cffc8c4a3b8ce
Darina Jurovskaja <darina@Darinas-MacBook-Pro.local>
2024-06-17 10:29:10 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,56 +1,63 @@
-const { getTopics } = require("./controllers/topics.controllers")
-const { getEndpoints } = require("./controllers/api.controllers")
-const { getArticleById, getArticles, getArticleCommentsById, postComment, patchVotes } = require("./controllers/articles.controllers")
-const { deleteComment } = require("./controllers/comments.controllers")
-const { getUsers } = require("./controllers/users.controllers")
-const topicsRouter = require("./routes/topics-router")
-const articleRouter = require("./routes/articles-router")
-const commentsRouter = require("./routes/comments-router")
+const { getTopics } = require("./controllers/topics.controllers");
+const { getEndpoints } = require("./controllers/api.controllers");
+const {
+  getArticleById,
+  getArticles,
+  getArticleCommentsById,
+  postComment,
+  patchVotes,
+} = require("./controllers/articles.controllers");
+const { deleteComment } = require("./controllers/comments.controllers");
+const { getUsers } = require("./controllers/users.controllers");
+const topicsRouter = require("./routes/topics-router");
+const articleRouter = require("./routes/articles-router");
+const commentsRouter = require("./routes/comments-router");
 
-const express = require("express")
-const usersRouter = require("./routes/users-router")
+const express = require("express");
+const usersRouter = require("./routes/users-router");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(cors());
 
-app.get("/api", getEndpoints)
+app.use(express.json());
 
-app.use("/api/topics", topicsRouter)
+app.get("/api", getEndpoints);
 
-app.use("/api/articles", articleRouter)
+app.use("/api/topics", topicsRouter);
 
-app.use("/api/comments", commentsRouter)
+app.use("/api/articles", articleRouter);
 
-app.use("/api/users", usersRouter)
+app.use("/api/comments", commentsRouter);
 
+app.use("/api/users", usersRouter);
 
-app.all('*', (req, res) => {
-    res.status(404).send({msg: "The request path does not exist"})
-})
-
-app.use((err, req, res, next) => {
-    if (err.code === '22P02') {
-        res.status(400).send({ msg: 'Invalid Input'} )
-    } else if (err.code === '23502') {
-        res.status(404).send({ msg: 'Article not found and comment cannot be posted'} )
-    } else if (err.code === '23503') {
-        res.status(400).send({ msg: 'This user does not exist'} )
-    } else next(err)
-})
+app.all("*", (req, res) => {
+  res.status(404).send({ msg: "The request path does not exist" });
+});
 
 app.use((err, req, res, next) => {
-    if (err.msg) {
-        res.status(err.status).send({msg: err.msg})
-    } else next(err)
-})
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Invalid Input" });
+  } else if (err.code === "23502") {
+    res
+      .status(404)
+      .send({ msg: "Article not found and comment cannot be posted" });
+  } else if (err.code === "23503") {
+    res.status(400).send({ msg: "This user does not exist" });
+  } else next(err);
+});
 
 app.use((err, req, res, next) => {
-    console.log(err)
-    res.status(500).send({ msg: 'Server Error! :(' })
-})
+  if (err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else next(err);
+});
 
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ msg: "Server Error! :(" });
+});
 
-
-
-module.exports = app
+module.exports = app;
